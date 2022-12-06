677fc6a7d381d6b36c642b156321e3f649c66094
Gyanendra Knojiya <gyanendrak064@gmail.com>
2022-12-06 22:45:00 +0530

    Day 6: Tuning Trouble

---
+++ 2022/Day 6/main.py
@@ -0,0 +1,28 @@
+def readInput():
+    inputList = open("./input.txt", "r").readlines()
+    input = "".join(inputList)
+    return input
+
+input = readInput()
+
+def findMarker(noOfSeqs):
+    a = 0
+    while a < (len(input) - noOfSeqs):
+        marker = input[a: a + noOfSeqs]
+        if len(set([*marker])) > noOfSeqs - 1:
+            return a + noOfSeqs
+        else:
+            a += 1
+
+
+def puzzle1():
+    return findMarker(4)
+
+
+def puzzle2():
+    return findMarker(14)
+
+
+print(puzzle1())
+
+print(puzzle2())
